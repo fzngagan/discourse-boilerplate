ee6197107e06eb825def038a3972bba442376fdc
Faizaan Gagan <fzngagan@gmail.com>
2020-12-17 14:18:37 +0530

    fixed code formatting and added plugin description

--- lib/plugin_init.rb
+++ lib/plugin_init.rb
@@ -1,17 +1,18 @@
+# frozen_string_literal: true
 module Boilerplate
-    class PluginInit
-      @@plugin_init = Proc.new {
-        puts "check enabled_site_setting"
-        puts respond_to? :enabled_site_setting
+  class PluginInit
+    @@plugin_init = Proc.new {
+      puts "check enabled_site_setting"
+      puts respond_to? :enabled_site_setting
 
-        after_initialize do
-            puts "check add_to_class"
-            puts respond_to? :add_to_class
-        end
-      }
-        def initialize(instance)
-            instance.instance_eval &@@plugin_init
-        end 
+      after_initialize do
+        puts "check add_to_class"
+          puts respond_to? :add_to_class
+      end
+    }
+
+    def initialize(instance)
+      instance.instance_eval &@@plugin_init
     end
+  end
 end
-
--- plugin.rb
+++ plugin.rb
@@ -1,16 +1,15 @@
 # frozen_string_literal: true
 
 # name: discourse-boilerplate
-# about: 
+# about: A discourse boilerplate for writing well structured plugins
 # version: 0.1
 # author: Faizaan Gagan
 # url: https://github.com/fzngagan/discourse-boilerplate
 
+[
+    "../lib/plugin_init.rb"
+].each do |path|
+  load File.expand_path(path, __FILE__)
+end
 
-    [
-        "../lib/plugin_init.rb"
-    ].each do |path|
-        load File.expand_path(path, __FILE__)
-        end
-
-        Boilerplate::PluginInit.new(self)
+Boilerplate::PluginInit.new(self)
